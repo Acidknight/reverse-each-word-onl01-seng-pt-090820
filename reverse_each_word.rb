0d3db2532b62cdd63ff2257451c3cedc5e3fb6f7
Anthony Leon <acidknights89@gmail.com>
2020-09-15 05:39:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,4 @@
 def reverse_each_word(string)
-  string = "Hi, again just making sure it's reversed!"
   statement = string.split(" ")
   words = []
   words = statement.collect {|word| word.reverse }
